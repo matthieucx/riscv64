15bc53cc393851b233484855dcb39a7a9dbba030
Matthieu <matthieu.cx@gmail.com>
2022-08-08 13:46:31 +0300

    Update build-go-riscv64.sh

--- build-go-riscv64.sh
+++ build-go-riscv64.sh
@@ -6,6 +6,7 @@ cd go/src
 
 # Integrate fix to support SV57 mode
 wget "https://go-review.googlesource.com/changes/go~409055/revisions/6/files/src%2Fruntime%2Flfstack_64bit.go/download" -O lfstack_fix_sv57.zip
+unzip lfstack_fix_sv57.zip
 mv lfstack_64bit_new-78164eeda52e46287233e7cb16f8514e.go lfstack_64bit.go
 mv lfstack_64bit.go runtime/lfstack_64bit.go
 
