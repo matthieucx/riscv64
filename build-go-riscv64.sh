e09a81eefdc5d332c083a779774e2a86bcf6b099
Matthieu <matthieu.cx@gmail.com>
2022-08-08 12:34:46 +0300

    Update build-go-riscv64.sh

--- build-go-riscv64.sh
+++ build-go-riscv64.sh
@@ -4,3 +4,7 @@ wget "https://go.dev/dl/go1.19.src.tar.gz"
 tar -xf go1.19.src.tar.gz
 cd go/src
 GOOS=linux GOARCH=riscv64 ./bootstrap.bash
+cd ../..
+mv go go1.19-src
+mv go-linux-riscv64-bootstrap go
+tar -cJf go1.19.linux-riscv64.tar.xz go/
